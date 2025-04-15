1581a28a6f8aece7276a46479ea9d0c7d3a8f4c3
wang <98175070+qifeikeji@users.noreply.github.com>
2025-04-15 08:11:34 +0000

    Update chromium-snap.sh

--- chromium-snap.sh
+++ chromium-snap.sh
@@ -1,5 +1,5 @@
 _create_chromium_appimage() {
-	# 下载和解压 Snap 包（保持不变）
+	# 下载和解压 Snap 包
 	if ! test -f ./*.snap; then
 		if wget --version | head -1 | grep -q ' 1.'; then
 			wget -q --no-verbose --show-progress --progress=bar "$(curl -H 'Snap-Device-Series: 16' http://api.snapcraft.io/v2/snaps/info/chromium --silent | sed 's/\[{/\n/g; s/},{/\n/g' | grep -i "$CHANNEL" | head -1 | sed 's/[()",{} ]/\n/g' | grep "^http")"
@@ -34,17 +34,18 @@ _create_chromium_appimage() {
 	sed -i 's/Exec=chromium/Exec=AppRun/g' ./"$APP".AppDir/*.desktop
 
 	# 生成 AppRun，使用动态路径
-	cat <<HEREDOC >> ./"$APP".AppDir/AppRun
-	#!/bin/sh
-	HERE="\$(dirname "\$(readlink -f "\${0}")")"
-	export UNION_PRELOAD="\${HERE}"
-	export PATH="\${HERE}"/usr/bin/:"\${HERE}"/usr/sbin/:"\${HERE}"/usr/games/:"\${PATH}"
-	export LD_LIBRARY_PATH="\${HERE}"/usr/lib/:"\${HERE}"/usr/lib/i386-linux-gnu/:"\${HERE}"/usr/lib/x86_64-linux-gnu/:"\${HERE}"/lib/:"\${HERE}"/lib/i386-linux-gnu/:"\${HERE}"/lib/x86_64-linux-gnu/:"\${LD_LIBRARY_PATH}"
-	export PYTHONPATH="\${HERE}"/usr/share/pyshared/:"\${HERE}"/usr/lib/python*/:"\${PYTHONPATH}"
-	export PYTHONHOME="\${HERE}"/usr/:"\${HERE}"/usr/lib/python*/
-	export XDG_DATA_DIRS="\${HERE}"/usr/share/:"\${XDG_DATA_DIRS}"
-	exec \${HERE}${CHROME_DIR#$APP.AppDir}/test "\$@"
-	HEREDOC
+	cat <<HEREDOC > ./"$APP".AppDir/AppRun
+#!/bin/sh
+HERE="\$(dirname "\$(readlink -f "\${0}")")"
+export UNION_PRELOAD="\${HERE}"
+export PATH="\${HERE}/usr/bin:\${HERE}/usr/sbin:\${HERE}/usr/games:\${PATH}"
+export LD_LIBRARY_PATH="\${HERE}/usr/lib:\${HERE}/usr/lib/i386-linux-gnu:\${HERE}/usr/lib/x86_64-linux-gnu:\${HERE}/lib:\${HERE}/lib/i386-linux-gnu:\${HERE}/lib/x86_64-linux-gnu:\${LD_LIBRARY_PATH}"
+export PYTHONPATH="\${HERE}/usr/share/pyshared:\${HERE}/usr/lib/python*:\${PYTHONPATH}"
+export PYTHONHOME="\${HERE}/usr:\${HERE}/usr/lib/python*"
+export XDG_DATA_DIRS="\${HERE}/usr/share:\${XDG_DATA_DIRS}"
+exec "\${HERE}${CHROME_DIR#$APP.AppDir}/test" "\$@"
+HEREDOC
+
 	chmod a+x ./"$APP".AppDir/AppRun
 
 	ARCH=x86_64 ./appimagetool --comp zstd --mksquashfs-opt -Xcompression-level --mksquashfs-opt 20 \
